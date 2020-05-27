ff6eb238c4e209347149301af6b5f0758969d5ce
yangxun <983323204@qq.com>
2020-05-27 22:36:53 +0800

    学习网络开发

---
+++ chapter1/echo_client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace echo_client
+{
+    class Program
+    {
+        static Socket socket;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("按下任意键开始连接...");
+            Console.ReadKey();
+            try
+            {
+                Connect();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("按下任意键退出...");
+            Console.ReadKey();
+        }
+
+        static void Connect()
+        {
+            socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            socket.Connect("127.0.0.1",8888);
+
+            var str = "你好";
+            var bytes = Encoding.UTF8.GetBytes(str);
+            socket.Send(bytes);
+            Console.WriteLine($"发送:{str}");
+            var buffer = new byte[1024];
+            var cnt = socket.Receive(buffer);
+            var recStr = Encoding.UTF8.GetString(buffer,0,cnt);
+            Console.WriteLine($"收到:{recStr}");
+            socket.Close();
+        }
+    }
+}
---
+++ chapter1/echo_server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace echo_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listener = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            var ip = IPAddress.Parse("127.0.0.1");
+            var ep = new IPEndPoint(ip,8888);
+            listener.Bind(ep);
+            listener.Listen(0);
+            Console.WriteLine("开始监听");
+            while(true)
+            {
+                var socket = listener.Accept();
+                Console.WriteLine($"接受连接：{socket.RemoteEndPoint}");
+                var buffer = new byte[1024];
+                var cnt = socket.Receive(buffer);
+                var str = Encoding.UTF8.GetString(buffer,0,cnt);
+                Console.WriteLine($"收到：{str}");
+                var echoStr = $"我已收到你发的{str}";
+                socket.Send(Encoding.UTF8.GetBytes(echoStr));
+            }
+        }
+    }
+}
